cfd84c388d6665721adc169f6a43ac5f73c81d13
moks <markwayne.declaro@gmail.com>
2025-02-04 01:30:33 +1100

    cleanup of unused imports

--- src/main/java/com/miguel/questionapi/controller/ParameterController.java
+++ src/main/java/com/miguel/questionapi/controller/ParameterController.java
@@ -4,7 +4,6 @@ import com.miguel.questionapi.service.ParameterService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
