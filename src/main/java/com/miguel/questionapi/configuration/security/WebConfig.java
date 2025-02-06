803d83a99af3c0d13c669f8a1246d42ef3c50686
moks <markwayne.declaro@gmail.com>
2025-02-06 23:16:25 +1100

    Corrected CORS config

--- src/main/java/com/miguel/questionapi/configuration/security/WebConfig.java
+++ src/main/java/com/miguel/questionapi/configuration/security/WebConfig.java
@@ -1,6 +1,6 @@
 package com.miguel.questionapi.configuration.security;
 
-import org.springframework.context.annotation.Configuration;
+/*import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,9 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Allow all endpoints
                 .allowedOrigins("http://localhost:5173")  // Allow your frontend's origin
-                .allowedMethods("GET", "POST", "PUT", "DELETE")  // Allow specific HTTP methods
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")  // Allow specific HTTP methods
                 .allowedHeaders("*")  // Allow any headers
                 .allowCredentials(true);  // Allow credentials (cookies, authorization headers)
     }
-}
+}*/
 
